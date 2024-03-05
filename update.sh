d81af74b30fd8b9e2ad44a3cbcc0f99fc93304f6
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-03-05 05:41:31 -0500

    Initial commit

---
+++ update.sh
@@ -0,0 +1,31 @@
+function header() {
+    echo -e "\033[0;100m$1\033[0m\n"
+}
+function success() {
+    echo -e "\033[0;42m$1\033[0m\n"
+}
+function failure() {
+    echo -e "\033[0;41m$1\033[0m\n"
+}
+
+function section() {
+    header "Updating '$1'"
+
+    for ((index = 2; index <= $#; index++)); do
+        command="${!index}"
+
+        if ! $command; then
+            failure "Failed to update '$1'"
+
+            return $?
+        fi
+
+        echo ""
+    done
+
+    success "Successfully updated '$1'"
+
+    return 0
+}
+
+section "apt" "sudo apt update" "sudo apt upgrade -qy" "sudo apt autoremove -qy"
